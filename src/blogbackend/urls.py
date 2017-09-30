9f64b385f05de06dd1cd5ad28c672b30d1575334
Rosendo Chas <rosendo@mood481.es>
2017-09-30 13:25:44 +0200

    Endpoints to check user name and email with mock data

--- src/blogbackend/urls.py
+++ src/blogbackend/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
+
+from usermanagement.views import checkUsername
+from usermanagement.views import checkUseremail
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+   url(r'^check/user/name/', checkUsername),
+   url(r'^check/user/email/', checkUseremail),
 ]
--- src/usermanagement/views.py
+++ src/usermanagement/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+
+def checkUsername(request):
+    if request.GET.get('name', '') == '':
+        flag = False
+    else:
+        flag = True
+    return JsonResponse({'result': flag})
+
+def checkUseremail(request):
+    if request.GET.get('email', '') == '':
+        flag = False
+    else:
+        flag = True
+    return JsonResponse({'result': flag})
